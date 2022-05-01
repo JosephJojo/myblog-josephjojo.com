6f2c282cc0d0bc608cbf6097cec1b6ee1dacdbb4
JosephJojo <josephjojojohn@gmail.com>
2022-05-02 02:47:17 +0530

    - breadcrumbs, Hero and Postcard - layouts in index.tsx Postcard design

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,39 @@ import { getPosts } from '../services'
 
 const Home: NextPage = ({ posts }) => {
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
+    <div className="container mx-auto px-5 md:px-10 2xl:px-28 mb-8 min-h-screen">
+      <title>Joseph's Blog</title>
+            <link rel="icon" href="/favicon.ico" />
+
+      <div className="mb-8 w-full">
+        <div className="text-sm breadcrumbs">
+          <ul>
+            <li><a>Home</a></li>
+            <li><a>Documents</a></li>
+            <li>Add Document</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 xl:grid-cols-12 gap-12 mb-8">
+
+        {/* Main Contents */}
+        <div className='xl:col-span-8 col-span-1'>
+          
+          {/* Section - Hero */}
+          <div className="hero card h-96 mb-8 cursor-pointer hero-bg">
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-secondary-content">
+              <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">Hello there 👋</h1>
+                <p className="mb-5">I'm a passionate about M365 web development, migration and the world around it. This blog serves as a place to document everything I've learned, developed, and challenges I've encountered between my career.</p>
+              </div>
+            </div>
+          </div>
+          
+          {/* Section Posts */}
           {posts.map((post, index) => <PostCard post={post.node} key={index} /> )}
+
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
