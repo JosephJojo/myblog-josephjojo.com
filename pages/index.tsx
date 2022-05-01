a44fcf340da1b5478ba1f1b4e82fa2a66639c84f
JosephJojo <josephjojojohn@gmail.com>
2022-05-02 01:21:21 +0530

    Service to read contents from graph cms - services/index.js Reading posts in index.tsx using getStaticPorps()

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing' },
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
-]
-
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -18,7 +14,7 @@ const Home: NextPage = () => {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => <PostCard post={post} key={post.title} /> )}
+          {posts.map((post, index) => <PostCard post={post.node} key={index} /> )}
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
@@ -33,3 +29,11 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export async function getStaticProps() {
+  const posts = (await getPosts() || [])
+
+  return {
+    props: { posts }
+  }
+}
