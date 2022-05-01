9a75236bd2ce4aa3b428d77871f8e1bfc7d637ac
JosephJojo <josephjojojohn@gmail.com>
2022-05-02 00:18:31 +0530

    Additions and changes
    New Components
     - Categories
     - PostCards
     - PostWidget
    
     Changes
      - git ignore - env reference
      - tailwind config
        > typography plugin
        > daisyui plugin

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+import { PostCard, Categories, PostWidget } from '../components'
+
 const posts = [
   { title: 'React Testing', excerpt: 'Learn React Testing' },
   { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
@@ -9,22 +11,20 @@ const posts = [
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-white">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))}
+          {posts.map((post, index) => <PostCard post={post} key={post.title} /> )}
         </div>
         <div className='lg:col-span-4 col-span-1'>
-          <div className='lg:sticky relative top-8'></div>
+          <div className='lg:sticky relative top-8'>
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
       
