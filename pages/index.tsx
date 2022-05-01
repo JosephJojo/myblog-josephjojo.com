ef035e991801263007c3f33ae05ed59491fa5e29
JosephJojo <josephjojojohn@gmail.com>
2022-05-02 03:23:41 +0530

    new component - AboutMe changed favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-import { PostCard, Categories, PostWidget } from '../components'
+import { PostCard, Categories, PostWidget, AboutMe } from '../components'
 import { getPosts } from '../services'
 
 const Home: NextPage = ({ posts }) => {
@@ -41,12 +41,18 @@ const Home: NextPage = ({ posts }) => {
           {posts.map((post, index) => <PostCard post={post.node} key={index} /> )}
 
         </div>
-        <div className='lg:col-span-4 col-span-1'>
+
+        {/* Right Pane -Start */}
+        <div className="xl:col-span-4 col-span-1">
+          <AboutMe />
+        </div>
+
+        {/* <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
             <PostWidget />
             <Categories />
           </div>
-        </div>
+        </div> */}
       </div>
       
     </div>
