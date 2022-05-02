1ea79ae3070958200e3cee6e3a592c63d8edb11f
JosephJojo <josephjojojohn@gmail.com>
2022-05-02 15:59:09 +0530

    slug page - Adjacent Posts - design

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -51,7 +51,31 @@ const PostDetails = ({ post }) => {
 						</div>
 					</div>
 
-					
+					{/* Adjacent Posts */}
+					<div className="card w-full bg-base-100 shadow-xl image-full mb-8 cursor-pointer">
+						<img className="w-full h-52 object-cover" src="https://api.lorem.space/image/shoes?w=400&h=225" />
+						<div className="card-body flex-row items-center navbar-start w-full">
+							<button className="btn btn-square btn-sm">
+								<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"> <path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" /> </svg>
+							</button>
+							<div>
+								<h2 className="card-title">Previous Post</h2>
+								<p>Oct 11, 2021</p>
+							</div>
+						</div>
+					</div>
+					<div className="card w-full bg-base-100 shadow-xl image-full mb-8 cursor-pointer">
+						<img className="w-full h-52 object-cover" src="https://api.lorem.space/image/shoes?w=400&h=225" />
+						<div className="card-body flex-row items-center navbar-end w-full">
+							<div>
+								<h2 className="card-title">Next Post</h2>
+								<p>Oct 22, 2021</p>
+							</div>
+							<button className="btn btn-square btn-sm">
+								<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"> <path stroke-linecap="round" stroke-linejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" /> </svg>
+							</button>
+						</div>
+					</div>
 
 				</div>
 
