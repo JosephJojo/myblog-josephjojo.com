be2d8075c0529cbcf68c6ec825e8070b2ebd8946
JosephJojo <josephjojojohn@gmail.com>
2022-05-08 21:53:29 +0530

    New slug - Category Page

---
+++ pages/category/[slug].js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+import { getCategories, getCategoryPost } from '../../services';
+import { PostCard, Categories, Loader } from '../../components'
+
+const CategoryPost = ({ posts }) => {
+    const router = useRouter();
+
+    if (router.isFallback) {
+        return <Loader />;
+    }
+
+    return (
+        <div>
+            {posts.map((post, index) => (
+                <PostCard key={index} post={post.node} />
+            ))}
+            <Categories />
+        </div>
+    )
+}
+
+export default CategoryPost
+
+export async function getStaticProps({ params }) {
+    const posts = await getCategoryPost(params.slug);
+
+    return {
+        props: { posts }
+    };
+}
+
+export async function getStaticPaths() {
+    const categories = await getCategories();
+    return {
+        paths: categories.map(({ slug }) => ({ params: { slug } })),
+        fallback: true,
+    };
+}
