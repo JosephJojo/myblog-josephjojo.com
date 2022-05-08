c5803611f397e2fca66da4ebd6e0813c57d44055
JosephJojo <josephjojojohn@gmail.com>
2022-05-09 00:27:01 +0530

    category page - design

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 import { getCategories, getCategoryPost, GetSlugName } from '../../services';
-import { PostCard, Categories, Loader } from '../../components'
+import { PostCard, Categories, Loader, AboutMe, PostWidget } from '../../components'
 
 const CategoryPost = ({ posts, slugName }) => {
     const router = useRouter();
@@ -25,10 +25,41 @@ const CategoryPost = ({ posts, slugName }) => {
 				</div>
 			</div>
 
-            {posts.map((post, index) => (
-                <PostCard key={index} post={post.node} />
-            ))}
-            <Categories />
+            <div className="grid grid-cols-1 xl:grid-cols-12 gap-12 mb-8">
+
+                {/* Main Contents */}
+                <div className='xl:col-span-8 col-span-1'>
+
+                    <div className="card shadow-xl mb-8">
+                        <div className='card-body'>
+                            <h2 className="card-title">{slugName}</h2>
+                        </div>
+                    </div>
+
+                    {posts.map((post, index) => (
+                        <PostCard key={index} post={post.node} />
+                    ))}
+                </div>
+
+                {/* Right Pane - Start */}
+				<div className="xl:col-span-4 col-span-1">
+                    <div className="relative xl:sticky top-8">
+
+                        {/* Section - About Me */}
+                        <AboutMe />
+
+                        {/* Section - Categories/Tags */}
+                        <Categories />
+
+                        {/* Section - Recent Posts */}
+                        <PostWidget slug={posts.slug} />
+
+                        </div>
+                </div>
+
+                
+            </div>
+
         </div>
     )
 }
